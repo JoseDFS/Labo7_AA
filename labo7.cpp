b7d4fce50d52b0031b6774fba89094e45694591f
Jose Segura <00001617@uca.edu.sv>
2019-10-21 22:04:01 -0600

    first commit

---
+++ labo7.cpp
@@ -0,0 +1,32 @@
+#include <limits.h>
+#include <stdio.h>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int max(int a, int b) { return (a > b)? a : b;}
+
+int cutRod(int price[], int n)
+{
+    int val[n+1];
+    val[0] = 0;
+    int i, j;
+    for (i = 1; i<=n; i++){
+        int max_val = -1;
+        for (j = 0; j < i; j++){
+            max_val = max(max_val, price[j] + val[i-(j+1)]);
+            val[i] = max_val;
+        }
+    }
+    return val[n];
+}
+
+
+int main()
+{
+    int arr[] = {1,5,8};
+    int size = sizeof(arr)/sizeof(arr[0]);
+    cout<<"Valor maximo obtenible: "<< cutRod(arr, size);
+    return 0;
+}
